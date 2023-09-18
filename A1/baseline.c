bf3936b04f647c4fcdda94a9ac7e469ebb783f11
Gerardo <gcerpa02@cox.net>
2023-09-18 15:48:23 -0700

    baseline

---
+++ A1/baseline.c
@@ -0,0 +1,16 @@
+
+#define N  100000000
+#define NUM_THREAD 2
+#define NUM_TASKS 2
+
+#include <stdio.h>
+
+int main(){
+    long long sum = 0;
+    for(int i = 0; i < N; i++){
+        sum += i;
+    }
+
+    printf("Total sum from 0 to %d is: %lld",N,sum);
+    return 0;
+}
