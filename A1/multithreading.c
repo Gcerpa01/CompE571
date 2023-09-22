29e20ee5867511b6b4d469f3eba3a0f6c46f593c
Gerardo <gcerpa02@cox.net>
2023-09-22 00:07:30 -0700

    run_times.sh for calculating averages, make to create executables and make clean_text to clean only txt files

--- A1/multithreading.c
+++ A1/multithreading.c
@@ -22,13 +22,16 @@ void* calculateSum(void* arg) {
     }
     data->sum = sum;
 
-    printf("Thread %d: Calculated sum from %d to %d: %lld\n",
-           data->threadID, data->start, data->end, data->sum);
+    // printf("Thread %d: Calculated sum from %d to %d: %lld\n",
+    //        data->threadID, data->start, data->end, data->sum);
 
     pthread_exit(NULL);
 }
 
 int main() {
+    clock_t start,end;
+    clock_t cpu_time_used;
+    start = clock();
     // Create the thread array
     pthread_t threads[NUM_THREADS];
 
@@ -43,14 +46,14 @@ int main() {
 
     // Create and start the threads
     for (int i = 0; i < NUM_THREADS; i++) {
-        printf("Creating Thread %d\n", i);
+        // printf("Creating Thread %d\n", i);
         pthread_create(&threads[i], NULL, calculateSum, &threadData[i]);
     }
 
     // Wait for the threads to finish
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
-        printf("Thread %d has finished\n", i);
+        // printf("Thread %d has finished\n", i);
     }
 
     // Now calculate the partition sums
@@ -59,8 +62,13 @@ int main() {
         finalSum += threadData[i].sum;
     }
 
+    end = clock();
+    cpu_time_used = end - start;
+    double cpu_seconds = (double)cpu_time_used/CLOCKS_PER_SEC;
+
     // Print the final result
     printf("Sum of numbers from 0 to %d is %lld\n", N, finalSum);
+    printf("Total CPU time taken in seconds: %.15f\n",cpu_seconds);
 
     return 0;
 }
--- A1/run_times.sh
+++ A1/run_times.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-count=5
+count=50
 commands=(
     "./baseline"
+    "./multithreading"
     "./multitasking"
+    
 )
 pattern="Total CPU time taken in seconds:"
 
 output_files=(
     "baseline_times.txt"
+    "multithreading_times.txt"
     "multitasking_times.txt"
 )
 
@@ -20,15 +23,16 @@ done
 
 for ((i = 0; i < ${#commands[@]};i++)); do
     command="${commands[i]}"
+    echo "Starting $count iteration of $command"
     output_file="${output_files[$i]}"
     total_time=0
     for((j=1; j<= $count; j++)); do
-        echo "Running $command - Attempt $j"
-        output="$($command)"
-        echo "$output" >> "$output_file"
+        # echo "Running $command - Attempt $j"
+        output="$($command)" #save output of execute
+        echo "$output" >> "$output_file" #save it to text file
         
         # Extract the CPU time from the output and accumulate it for averaging
-        cpu_time=$(echo "$output" | grep "$pattern" | awk '{print $NF}')
+        cpu_time=$(echo "$output" | grep "$pattern" | awk '{print $NF}') 
         total_time=$(awk "BEGIN {print $total_time + $cpu_time}")
         wait
     done
@@ -37,5 +41,5 @@ for ((i = 0; i < ${#commands[@]};i++)); do
     echo "Average CPU time for $command: $average_time seconds"
 done
 
-echo "Finished running all commands the specified number of times."
+echo "Finished running all commands for $count times each"
 
