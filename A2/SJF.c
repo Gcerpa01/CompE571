fb3fab900696370f427d14318312bcdf08927d04
Jarrod Rowson <jrowson7843@sdsu.edu>
2023-10-16 20:14:59 -0700

    SJF and init commit for A2

---
+++ A2/SJF.c
@@ -0,0 +1,156 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>  
+#include <stdlib.h>  
+#include <sys/wait.h> 
+#include <string.h> 
+#include <time.h> 
+#include <signal.h>
+#include <sys/time.h>
+
+/************************************************************************************************ 
+		These DEFINE statements represent the workload size of each task and 
+		the time quantum values for Round Robin scheduling for each task.
+*************************************************************************************************/
+
+#define WORKLOAD1 100000
+#define WORKLOAD2 50000
+#define WORKLOAD3 25000
+#define WORKLOAD4 10000
+
+#define QUANTUM1 1000
+#define QUANTUM2 1000
+#define QUANTUM3 1000
+#define QUANTUM4 1000
+
+/************************************************************************************************ 
+					DO NOT CHANGE THE FUNCTION IMPLEMENTATION
+*************************************************************************************************/
+void myfunction(int param){
+
+	int i = 2;
+	int j, k;
+
+	while(i < param){
+		k = i; 
+		for (j = 2; j <= k; j++)
+		{
+			if (k % j == 0){
+				k = k/j;
+				j--;
+				if (k == 1){
+					break;
+				}
+			}
+		}
+		i++;
+	}
+}
+/************************************************************************************************/
+
+int main(int argc, char const *argv[])
+{
+	pid_t pid1, pid2, pid3, pid4;
+	int running1, running2, running3, running4;
+
+	pid1 = fork();
+
+	if (pid1 == 0){
+
+		myfunction(WORKLOAD1);
+
+		exit(0);
+	}
+	kill(pid1, SIGSTOP);
+
+	pid2 = fork();
+
+	if (pid2 == 0){
+
+		myfunction(WORKLOAD2);
+
+		exit(0);
+	}
+	kill(pid2, SIGSTOP);
+
+	pid3 = fork();
+
+	if (pid3 == 0){
+
+		myfunction(WORKLOAD3);
+
+		exit(0);
+	}
+	kill(pid3, SIGSTOP);
+
+	pid4 = fork();
+
+	if (pid4 == 0){
+
+		myfunction(WORKLOAD4);
+
+		exit(0);
+	}
+	kill(pid4, SIGSTOP);
+
+	/************************************************************************************************ 
+		At this point, all  newly-created child processes are stopped, and ready for scheduling.
+	*************************************************************************************************/
+
+
+	/************************************************************************************************
+		- Scheduling code starts here
+	************************************************************************************************/
+
+    // Calculate execution times
+    int execution_times[] = {WORKLOAD1, WORKLOAD2, WORKLOAD3, WORKLOAD4};
+    pid_t processes[] = {pid1, pid2, pid3, pid4};
+    int num_processes = 4;
+
+    struct timeval start_times[num_processes];
+
+    // Print initial state // - FOR DEBUGGING
+    // printf("Initial state:\n");
+    // for (int i = 0; i < num_processes; i++) {
+    //     printf("Execution Time: %d, Process ID: %d\n", execution_times[i], processes[i]);
+    // }
+
+    // Sort processes based on execution times (SJF)
+    for (int i = 0; i < num_processes - 1; i++) {
+        for (int j = 0; j < num_processes - i - 1; j++) {
+            if (execution_times[j] > execution_times[j + 1]) {
+                // Swap execution times
+                int temp = execution_times[j];
+                execution_times[j] = execution_times[j + 1];
+                execution_times[j + 1] = temp;
+                // Swap corresponding processes
+                pid_t temp_pid = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp_pid;
+
+                // // Print state after swap // - FOR DEBUGGING
+                // printf("\nAfter Swap:\n");
+                // for (int k = 0; k < num_processes; k++) {
+                //     printf("Execution Time: %d, Process ID: %d\n", execution_times[k], processes[k]);
+                // }
+            }
+        }
+    }
+
+   // Start and resume processes in SJF order and calculate response time
+    for (int i = 0; i < num_processes; i++) {
+        gettimeofday(&start_times[i], NULL); // Record the start time
+        kill(processes[i], SIGCONT);
+        wait(NULL); // Wait for the child process to finish
+        struct timeval end_time;
+        gettimeofday(&end_time, NULL);
+        long response_time = (end_time.tv_sec - start_times[i].tv_sec) * 1000000 + (end_time.tv_usec - start_times[i].tv_usec);
+        printf("Process ID: %d, Response Time: %ld microseconds\n", processes[i], response_time);
+    }
+	
+	/************************************************************************************************
+		- Scheduling code ends here
+	************************************************************************************************/
+
+	return 0;
+}
---
+++ A2/sample/sample_program.c
@@ -0,0 +1,147 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>  
+#include <stdlib.h>  
+#include <sys/wait.h> 
+#include <string.h> 
+#include <time.h> 
+#include <signal.h>
+#include <sys/time.h>
+
+/************************************************************************************************ 
+		These DEFINE statements represent the workload size of each task and 
+		the time quantum values for Round Robin scheduling for each task.
+*************************************************************************************************/
+
+#define WORKLOAD1 100000
+#define WORKLOAD2 50000
+#define WORKLOAD3 25000
+#define WORKLOAD4 10000
+
+#define QUANTUM1 1000
+#define QUANTUM2 1000
+#define QUANTUM3 1000
+#define QUANTUM4 1000
+
+/************************************************************************************************ 
+					DO NOT CHANGE THE FUNCTION IMPLEMENTATION
+*************************************************************************************************/
+void myfunction(int param){
+
+	int i = 2;
+	int j, k;
+
+	while(i < param){
+		k = i; 
+		for (j = 2; j <= k; j++)
+		{
+			if (k % j == 0){
+				k = k/j;
+				j--;
+				if (k == 1){
+					break;
+				}
+			}
+		}
+		i++;
+	}
+}
+/************************************************************************************************/
+
+int main(int argc, char const *argv[])
+{
+	pid_t pid1, pid2, pid3, pid4;
+	int running1, running2, running3, running4;
+
+	pid1 = fork();
+
+	if (pid1 == 0){
+
+		myfunction(WORKLOAD1);
+
+		exit(0);
+	}
+	kill(pid1, SIGSTOP);
+
+	pid2 = fork();
+
+	if (pid2 == 0){
+
+		myfunction(WORKLOAD2);
+
+		exit(0);
+	}
+	kill(pid2, SIGSTOP);
+
+	pid3 = fork();
+
+	if (pid3 == 0){
+
+		myfunction(WORKLOAD3);
+
+		exit(0);
+	}
+	kill(pid3, SIGSTOP);
+
+	pid4 = fork();
+
+	if (pid4 == 0){
+
+		myfunction(WORKLOAD4);
+
+		exit(0);
+	}
+	kill(pid4, SIGSTOP);
+
+	/************************************************************************************************ 
+		At this point, all  newly-created child processes are stopped, and ready for scheduling.
+	*************************************************************************************************/
+
+
+
+	/************************************************************************************************
+		- Scheduling code starts here
+		- Below is a sample schedule. (which scheduling algorithm is this?)
+		- For the assignment purposes, you have to replace this part with the other scheduling methods 
+		to be implemented.
+	************************************************************************************************/
+
+	running1 = 1;
+	running2 = 1;
+	running3 = 1;
+	running4 = 1;
+
+	while (running1 > 0 || running2 > 0 || running3 > 0 || running4 > 0)
+	{
+		if (running1 > 0){
+			kill(pid1, SIGCONT);
+			usleep(QUANTUM1);
+			kill(pid1, SIGSTOP);
+		}
+		if (running2 > 0){
+			kill(pid2, SIGCONT);
+			usleep(QUANTUM2);
+			kill(pid2, SIGSTOP);
+		}
+		if (running3 > 0){
+			kill(pid3, SIGCONT);
+			usleep(QUANTUM3);
+			kill(pid3, SIGSTOP);
+		}
+		if (running4 > 0){
+			kill(pid4, SIGCONT);
+			usleep(QUANTUM4);
+			kill(pid4, SIGSTOP);
+		}
+		waitpid(pid1, &running1, WNOHANG);
+		waitpid(pid2, &running2, WNOHANG);
+		waitpid(pid3, &running3, WNOHANG);
+		waitpid(pid4, &running4, WNOHANG);
+	}
+
+	/************************************************************************************************
+		- Scheduling code ends here
+	************************************************************************************************/
+
+	return 0;
+}
